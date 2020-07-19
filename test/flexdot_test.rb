817e6ef3d6ec2cbb6242cce4bd911a631347c018
Katsuya Hidaka <hidakatsuya@gmail.com>
2020-07-20 03:10:05 +0900

    add minimum tests

--- test/flexdot_test.rb
+++ test/flexdot_test.rb
@@ -1,11 +1,33 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class FlexdotTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Flexdot::VERSION
+  include TestHelper
+
+  def setup
+    reset_test_dir
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_task
+    stdout = run_rake 'install:example'
+
+    assert_equal <<~OUT, stdout
+      [link_created] .arc
+      [link_created] b.conf (backup)
+      [link_created] config/ccc/c.json
+    OUT
+
+    stdout = run_rake 'install:example'
+
+    assert_equal <<~OUT, stdout
+      [already_linked] .arc
+      [already_linked] b.conf
+      [already_linked] config/ccc/c.json
+    OUT
+  end
+
+  def teardown
+    reset_test_dir
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,49 @@
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "flexdot"
+# frozen_string_literal: true
 
-require "minitest/autorun"
+require 'pathname'
+require_relative '../lib/flexdot'
+
+require 'minitest/autorun'
+
+module TestHelper
+  def root
+    @root ||= Pathname.new(__dir__)
+  end
+
+  def home
+    root / 'home'
+  end
+
+  def dotfiles
+    home / 'dotfiles'
+  end
+
+  def reset_test_dir
+    reset_home
+    reset_dotfiles
+  end
+
+  def reset_home
+    home.each_child do |path|
+      next if path == dotfiles
+
+      if path.directory?
+        FileUtils.rm_rf(path.to_s)
+      else
+        path.delete
+      end
+    end
+
+    home.join('b.conf').write('')
+  end
+
+  def reset_dotfiles
+    FileUtils.rm_rf(dotfiles.join('backup').to_s)
+  end
+
+  def run_rake(task)
+    Dir.chdir(dotfiles.to_path) do
+      `rake #{task}`
+    end
+  end
+end
